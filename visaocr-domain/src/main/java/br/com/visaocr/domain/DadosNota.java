443452237822f87064542d2c8aeebd93196265eb
Rodrigo Alexandre Mafra da Silva <rodmafra@Rodrigos-MacBook-Pro.local>
2016-09-19 23:24:55 -0300

    Status da nota

--- visaocr-domain/src/main/java/br/com/visaocr/domain/DadosNota.java
+++ visaocr-domain/src/main/java/br/com/visaocr/domain/DadosNota.java
@@ -11,5 +11,20 @@ public class DadosNota {
 	private String coo;
 	private String data;
 	private String valorTotal;
+	private StatusNota statusNota;
+	
+	public enum StatusNota {
+		PENDENTE_OCR(0), OK_OCR(1), ERRO_OCR(2), INCONSISTENTE_OCR(3);
+		
+		private int status;
+
+		private StatusNota(int status) {
+			this.status = status;
+		}
+
+		public int getStatus() {
+			return this.status;
+		}
+	}
 	
 }
