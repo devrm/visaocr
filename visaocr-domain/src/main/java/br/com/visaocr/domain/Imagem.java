594fed79af477ecbaf8a8610f25d11c9022862e4
Rodrigo Alexandre Mafra da Silva <rodmafra@Rodrigos-MacBook-Pro.local>
2016-10-17 23:18:33 -0200

    Refazendo o tratamento de imagem e caminho do resultado.

--- visaocr-domain/src/main/java/br/com/visaocr/domain/Imagem.java
+++ visaocr-domain/src/main/java/br/com/visaocr/domain/Imagem.java
@@ -1,9 +1,30 @@
 package br.com.visaocr.domain;
 
+import java.io.File;
+
 import lombok.Data;
 
 @Data
 public class Imagem {
+	
 	private Integer id;
 	private String caminho;
+	private String caminhoDestino;
+	
+	public Imagem(String caminho) {
+		this.caminho = caminho;
+	}
+	
+	
+	public String extraiNomeImagem() {
+		
+		if (caminho == null) {
+			return "";
+		} else {
+			return caminho.substring(caminho.lastIndexOf(File.separatorChar)+1, caminho.length()); 
+		}
+		
+	}
+	
+	
 }
